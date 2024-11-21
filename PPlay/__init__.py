cef758136bd06647199bdbad7f92e5eb9e30c40b
Rottschaefer <rottschaefer54@gmail.com>
2024-11-21 20:01:23 -0300

    Inter

--- PPlay/__init__.py
+++ PPlay/__init__.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 __all__ =   [
             "animation",
             "collision",
@@ -10,3 +11,17 @@ __all__ =   [
             "sprite",
             "window"
             ]
+=======
+__all__ =   [
+            "animation",
+            "collision",
+            "gameimage",
+            "gameobject",
+            "keyboard",
+            #"mouse",
+            "point",
+            #"sound",
+            "sprite",
+            "window"
+            ]
+>>>>>>> 1c73ac2717b15f72afa3c2c99444a2543c94e074
