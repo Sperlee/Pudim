9df5bfefaef3c27180fc028a2b33504f15bcba2d
Rottschaefer <rottschaefer54@gmail.com>
2025-01-03 18:03:27 -0300

    Se o player soltar as caixas acima do piso elas caem com uma certa aceleracao ate encontrarem o chao ou outra caixa

--- fases/fase1/fase1.py
+++ fases/fase1/fase1.py
@@ -7,6 +7,9 @@ from PPlay.sprite import Sprite
 class Fase1:
     def __init__(self):
         self.julia = Julia(basic_setup.janela, 300, basic_setup.janela.height - 113 - 143 + 5, 200, 150, 100)
+
+
+        #Elementos da cena
         self.piso = GameImage("fases/fase1/imagens/piso.png")
         self.piso.set_position(0, basic_setup.janela.height - self.piso.height)
 
@@ -27,19 +30,29 @@ class Fase1:
 
         self.conteiner = GameImage("fases/fase1/imagens/conteiner.png")
         self.conteiner.set_position(basic_setup.janela.width/2 - self.conteiner.width/2, self.piso.y - self.conteiner.height + 5)
+        
 
+        #Elementos do guindaste
         self.cabo_guindaste = Sprite("fases/fase1/imagens/guindaste/cabo_guindaste.png")
         self.cabo_guindaste.set_position(self.guindaste_superior.x + 1000, self.guindaste_superior.y)
 
         self.gancho = Sprite("fases/fase1/imagens/guindaste/gancho.png")
         self.gancho.set_position(self.cabo_guindaste.x - 7, self.cabo_guindaste.y + 602)
 
-        self.guindaste_speed = 200
+        self.guindaste_speed = 400
 
+        #Lista de imagens para renderizar a uma
         self.images = [self.piso,self.conteiner, self.coluna_guindaste_esquerda, self.coluna_guindaste_direita, self.cabo_guindaste, self.gancho, self.guindaste_superior, self.painel_de_controle, self.placa_aviso]
 
+        #Elementos das caixas
         self.caixas = [Sprite("fases/fase1/imagens/caixa1.png"), Sprite("fases/fase1/imagens/caixa2.png"), Sprite("fases/fase1/imagens/caixa3.png"), Sprite("fases/fase1/imagens/caixa4.png"), Sprite("fases/fase1/imagens/caixa5.png")]
 
+        self.movendo_caixa = [False, -1] #a segunda posicao é para saber qual caixa esta sendo movida
+        self.caixa_caindo = [False, -1]
+        self.caixa_caindo_speed = 400
+
+
+        #Elementos das divisorias das caixas
         self.divs = [GameImage("fases/fase1/imagens/divisoria.png"), GameImage("fases/fase1/imagens/divisoria.png"), GameImage("fases/fase1/imagens/divisoria.png"), GameImage("fases/fase1/imagens/divisoria.png")]
 
         for i in range(len(self.divs)):
@@ -47,7 +60,10 @@ class Fase1:
 
         self.play_mode = 0 # 0 = jogo normal na plataforma ,  1 = jogo pelo guindaste
 
-        self.movendo_caixa = [False, -1] #a segunda posicao é para saber qual caixa esta sendo movida
+
+
+        self.texto_guindaste = GameImage("fases/fase1/imagens/texto_guindaste.png")
+        self.texto_manual = GameImage("fases/fase1/imagens/manual.png")
 
     def draw_images(self):
         for image in self.images:
@@ -85,6 +101,24 @@ class Fase1:
             if(basic_setup.teclado.key_pressed("ESC")):
                 self.play_mode = 0
 
+    def mover_caixa(self):
+            
+            index = self.movendo_caixa[1]
+            
+            limite_inferior = (self.caixas[index].y + self.caixas[index].width <= self.piso.y) or (basic_setup.teclado.key_pressed("UP"))
+
+            limite_superior = (self.caixas[index].y >= self.guindaste_superior.y + self.guindaste_superior.height + 30) or (basic_setup.teclado.key_pressed("DOWN"))
+
+            if(limite_superior and limite_inferior):
+                self.caixas[index].move_key_y(self.guindaste_speed*basic_setup.janela.delta_time())
+
+
+            limite_esquerdo = (self.caixas[index].x >= self.guindaste_superior.x + 200) or (basic_setup.teclado.key_pressed("RIGHT"))
+            limite_direito = (self.caixas[index].x <= self.guindaste_superior.x + self.guindaste_superior.width - 100) or (basic_setup.teclado.key_pressed("LEFT"))
+
+            if(limite_esquerdo and limite_direito):
+                self.caixas[index].move_key_x(self.guindaste_speed*basic_setup.janela.delta_time())
+
     def qual_divisoria(self, caixa):
 
         div1 = -1
@@ -107,7 +141,7 @@ class Fase1:
             div1 = self.qual_divisoria(self.caixas[index])
             div2 = self.qual_divisoria(self.caixas[i-1])
 
-            if div1 == -1 or div2 == -1 or div1 == div2:
+            if (div1 == -1 or div2 == -1 or div1 == div2) and not (self.caixas[index].x > self.caixas[i-1].x and self.caixas[index].x + self.caixas[index].width < self.caixas[i-1].x + self.caixas[i-1].width):
                 return False
         return True
 
@@ -141,14 +175,13 @@ class Fase1:
             self.draw_images()
 
             
-                
-            
             match self.play_mode:
                 case 0:
                     self.julia.andar(self.julia.x, self.julia.y)
 
                     if self.julia.x > self.coluna_guindaste_esquerda.x and self.julia.x < self.coluna_guindaste_esquerda.x + self.coluna_guindaste_esquerda.width - 30:
-                        basic_setup.janela.draw_text("Pressione espaço para jogar pelo guindaste", 200, self.conteiner.y - 70, 50, (255, 255, 255), "Arial", True)
+                        self.texto_guindaste.set_position(110, self.conteiner.y - 70)
+                        self.texto_guindaste.draw()
 
 
                     if basic_setup.teclado.key_pressed("SPACE"):
@@ -162,53 +195,47 @@ class Fase1:
                             self.julia.pular(self.julia.x, self.julia.y, self.draw_images)
 
                 case 1:
+                    self.texto_manual.set_position(150, self.conteiner.y - 70)
+                    self.texto_manual.draw()
 
                     self.mover_guindaste()
 
                     for index, caixa in enumerate(self.caixas):
-                        if(self.gancho.collided(caixa) and basic_setup.teclado.key_pressed("C") and not self.movendo_caixa[0] ):
+                        if(self.gancho.collided(caixa) and basic_setup.teclado.key_pressed("P") and not self.movendo_caixa[0] and not self.caixa_caindo[0]):
                            
                             if self.pode_pegar_caixa(index):
                                 self.movendo_caixa = [True, index]
 
-                        elif basic_setup.teclado.key_pressed("D") and self.movendo_caixa[0]:
-                            if self.pode_soltar_caixa(self.movendo_caixa[1]):
-                                self.movendo_caixa = [False, -1]
-                            
+                        elif self.caixa_caindo[0] and index != self.caixa_caindo[1] and self.caixas[self.caixa_caindo[1]].collided(caixa):
+                            self.caixa_caindo = [False, -1]
+                            self.caixa_caindo_speed = 200
 
 
+            
+
+                    if basic_setup.teclado.key_pressed("S") and self.movendo_caixa[0] and self.pode_soltar_caixa(self.movendo_caixa[1]):
+                        
+                        self.caixa_caindo = [True,  self.movendo_caixa[1]]
+
+                        self.movendo_caixa = [False, -1]
 
 
-                        # print(self.movendo_caixa)
+                    if self.caixa_caindo[0]:
+                        self.caixas[self.caixa_caindo[1]].y += (self.caixa_caindo_speed*basic_setup.janela.delta_time())
 
-                        if (self.movendo_caixa[0] and index == self.movendo_caixa[1]) and (not caixa.collided(self.guindaste_superior) or basic_setup.teclado.key_pressed("DOWN")) and (not caixa.collided(self.piso) or basic_setup.teclado.key_pressed("UP")):
-                            caixa.move_key_x(self.guindaste_speed*basic_setup.janela.delta_time())
-                            caixa.move_key_y(self.guindaste_speed*basic_setup.janela.delta_time())
+                        self.caixa_caindo_speed += 500*basic_setup.janela.delta_time()
 
-                        # elif self.movendo_caixa[0]  and (self.movendo_caixa[1] ==  0 or ((abs(self.caixas[self.movendo_caixa[1]].x - self.caixas[self.movendo_caixa[1]-1].x) > 300) and (self.movendo_caixa[1]< index and self.caixas[self.movendo_caixa[1]].y < self.caixas[self.movendo_caixa[1]+ 1].y))):
-                        #     basic_setup.janela.draw_text("Aqui tu pode soltar a caixa", 200, self.conteiner.y - 70, 50, (255, 255, 255), "Arial", True)
-                        #     if basic_setup.teclado.key_pressed("D"):
-                        #         self.movendo_caixa = [False, -1]
+                        if self.caixas[self.caixa_caindo[1]].y >= self.piso.y - self.caixas[self.caixa_caindo[1]].height:
+                            self.caixa_caindo = [False, -1]
+                            self.caixa_caindo_speed = 200
 
-
-                        # else:
-                        #     basic_setup.janela.draw_text("Aqui tu NAO pode soltar a caixa", 200, self.conteiner.y - 70, 50, (255, 255, 255), "Arial", True)
-
-
-                   
+                    elif (self.movendo_caixa[0]):
+                        self.mover_caixa()
 
                     
-                    # print("jogo pelo guindaste")
-                
-            
-
-            
-
             self.julia.draw()
             basic_setup.janela.update()
 
 
-
-        
 
 fase_1 = Fase1()
